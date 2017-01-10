154677fed1b17b63ff14db2aa0f9a8f4033e3d08
skulegirl <anna@egate.net>
2017-01-10 12:00:39 -0500

    Change NL zip code encoding to remove space

--- src/postal/forms/nl/forms.py
+++ src/postal/forms/nl/forms.py
@@ -1,14 +1,25 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.utils import six
 from localflavor.nl.forms import NLZipCodeField
 
 from postal.forms import PostalAddressForm
+
+class MyNLZipCodeField(NLZipCodeField):
+    def clean(self, value):
+        if isinstance(value, six.string_types):
+            value = value.upper().replace(' ', '')
+
+        # don't strip the spaces out of the zipcode, it confuses
+        # the geocoders
+        return super(NLZipCodeField, self).clean(value)
+ 
 
 class NLPostalAddressForm(PostalAddressForm):    
     line1 = forms.CharField(label=_(u"Street"), max_length=100)
     line2 = forms.CharField(label=_(u"Area"), required=False, max_length=100)
     city = forms.CharField(label=_(u"Town/City"), max_length=100)
-    code = NLZipCodeField(label=_(u"Zip Code"))
+    code = MyNLZipCodeField(label=_(u"Zip Code"))
     
     
     class Meta:
